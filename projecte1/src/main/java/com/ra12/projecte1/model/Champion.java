9a85443ffaed0937510e51e9ba209b6ee4a4aac0
emdickim <kimlazaropardo@gmail.com>
2026-02-08 20:57:25 +0100

    Fet controller i afegides dependencies jpa :D

--- projecte1/src/main/java/com/ra12/projecte1/model/Champion.java
+++ projecte1/src/main/java/com/ra12/projecte1/model/Champion.java
@@ -1,5 +1,101 @@
 package com.ra12.projecte1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Champion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String title;
+    private String role;
+    private String difficulty;
+    private LocalDate releaseDate;
+    private String imageUrl;
+    private String bio;
+
+    public Champion() {
+    }
+
+    public Champion(String name, String title, String role, String difficulty, LocalDate releaseDate, String imageUrl, String bio) {
+        this.name = name;
+        this.title = title;
+        this.role = role;
+        this.difficulty = difficulty;
+        this.releaseDate = releaseDate;
+        this.imageUrl = imageUrl;
+        this.bio = bio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
     
 }
